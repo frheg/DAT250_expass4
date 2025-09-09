ff57a05c8a938b9087680ce8cb447961a97d3731
Maren <maren.franke24@gmx.de>
2025-09-09 14:48:48 +0200

    Add bruno files and user management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,46 +5,90 @@ import './App.css'
 import Poll from "./components/Poll.jsx";
 import CreatePoll from "./components/CreatePoll.jsx";
 
+const USER_ID = "user1"
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
     const [polls, setPolls] = useState([])
+    const [userId, setUserId] = useState(localStorage.getItem('userId'))
 
     useEffect(() => {
-       fetchPolls()
+        manageUser()
+
+        fetchPolls()
     }, []);
 
 
-  async function fetchPolls() {
-      const response = await fetch("http://localhost:8080/polls");
-      if (!response.ok) throw new Error("Network error");
-      const data = await response.json();
+    async function manageUser() {
+        const checkUserExists = await requestUser(userId);
 
-      // if it's an array
-      for (const poll of data) {
-          console.log(poll.pollId, poll.question, poll.publishedAt);
-      }
-      console.log(data)
-      setPolls(data)
-  }
+        console.log('User Exists', checkUserExists)
 
-  async function createNewPoll(poll) {
-      const response = await fetch("http://localhost:8080/polls", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(poll)
-      });
+        // Test if user already exists
+        if (userId == null || checkUserExists == null) {
+            const newUser = createNewUser({userId: USER_ID, email: 'miau@gmx.no', password: 'miaumiau'})
 
-      if (!response.ok) throw new Error("Failed to submit vote");
+            localStorage.setItem('userId', USER_ID)
+            setUserId(USER_ID)
 
-      const data = await response.json();
-      console.log(data)
+            fetchPolls()
+        }
+    }
 
-      fetchPolls()
-  }
+    async function fetchPolls() {
+        const response = await fetch("http://localhost:8080/polls");
+        if (!response.ok) throw new Error("Network error");
+        const data = await response.json();
 
+        // if it's an array
+        for (const poll of data) {
+            console.log(poll.pollId, poll.question, poll.publishedAt);
+        }
+        console.log(data)
+        setPolls(data)
+    }
+
+    async function createNewPoll(poll) {
+        const response = await fetch("http://localhost:8080/polls", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(poll)
+        });
+
+        if (!response.ok) throw new Error("Failed to submit vote");
+
+        const data = await response.json();
+        console.log(data)
+
+        fetchPolls()
+    }
+
+    async function createNewUser(user) {
+        const response = await fetch("http://localhost:8080/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        });
+
+        if (!response.ok) throw new Error("Failed to submit vote");
+
+        const data = await response.json();
+        console.log(`Created new user: ${data}`)
+    }
+
+    async function requestUser(userId) {
+        const response = await fetch("http://localhost:8080/users/" + userId);
+        if (!response.ok) return null;
+
+        const data = await response.json();
+        console.log(`Created new user: ${data}`)
+        return data
+    }
 
     async function newVote(vote) {
         const response = await fetch("http://localhost:8080/votes", {
@@ -63,15 +107,15 @@ function App() {
         fetchPolls()
     }
 
-  return (
-    <>
-        {polls.map(poll => (
-            <Poll newVoteCallback={newVote} key={`poll_${poll.pollId}`} poll={poll} />
-        ))}
+    return (
+        <>
+            {polls.map(poll => (
+                <Poll newVoteCallback={newVote} key={`poll_${poll.pollId}`} poll={poll}/>
+            ))}
 
-        <CreatePoll createPollCallback={createNewPoll} />
-    </>
-  )
+            <CreatePoll createPollCallback={createNewPoll}/>
+        </>
+    )
 }
 
 export default App
